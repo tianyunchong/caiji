2e3ff80f85ed0d3af58cbcea54a200ca68936d22
tianyunchong <854024109@qq.com>
2017-03-14 16:36:26 +0800

    增加个人搜索工具init

---
+++ c/searchpeople/searchpeople/html/html.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include "html.h"
+void testPrintf() {
+    printf("%s\n", "aaaa");
+}
---
+++ c/searchpeople/searchpeople/html/html.h
@@ -0,0 +1,6 @@
+#ifndef HTML_H_INCLUDED
+#define HTML_H_INCLUDED
+
+
+void testPrintf();
+#endif // HTML_H_INCLUDED
